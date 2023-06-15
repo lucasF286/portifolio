bf097d36ffdeabef72c7a73c4c792f352dd7b01a
Lucas Freitas <lucasfsouzaa1@gmail.com>
2023-06-14 22:58:01 -0300

    inclusao icones cloudflare

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function animaTexto  (texto) {
+function animaTexto (texto) {
     const arrayTexto = texto.innerHTML.split('');
     texto.innerHTML = '';
     arrayTexto.forEach((letra, i) => {
         setTimeout(() => {
             texto.innerHTML += letra;
         }, 75*i)
-    }); 
+    });
 }
-
 
 const titulo = document.querySelector('.typing');
 
 animaTexto(titulo);
+
 
